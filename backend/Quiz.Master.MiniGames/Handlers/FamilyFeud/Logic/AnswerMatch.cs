2c8e57301543b04f63fb54a4d1c8cc8cdd900b1c
paczulapiotr <paczulapiotr@gmail.com>
2025-02-19 00:42:57 +0100

    fix fuzzy match

--- backend/Quiz.Master.MiniGames/Handlers/FamilyFeud/Logic/AnswerMatch.cs
+++ backend/Quiz.Master.MiniGames/Handlers/FamilyFeud/Logic/AnswerMatch.cs
@@ -5,20 +5,22 @@ namespace Quiz.Master.MiniGames.Handlers.FamilyFeud.Logic;
 
 public static class AnswerMatch
 {
-    public static (FamilyFeudDefinition.QuestionAnswer answer, string synonym)? MatchAnswer(IEnumerable<FamilyFeudDefinition.QuestionAnswer> answers, string answer, IEnumerable<string> usedAnswers)
+    public static (FamilyFeudDefinition.QuestionAnswer answer, string synonym)? MatchAnswer(IEnumerable<FamilyFeudDefinition.QuestionAnswer> answers, string answer, IEnumerable<string> usedAnswers, int threshold = 80)
     {
         var match = answers.Where(x => !usedAnswers.Contains(x.Id))
             .Select(x
                 => x.Synonyms
-                    .Select(s => new
-                    {
-                        Ratio = Fuzz.Ratio(s, answer),
-                        Answer = x,
-                        Synonym = s
+                    .Select(s => {
+                        var ratio = Fuzz.Ratio(s, answer);
+                        return new
+                        {
+                            Ratio = ratio,
+                            Answer = x,
+                            Synonym = s
+                        };
                     })
                     .OrderByDescending(x => x.Ratio).FirstOrDefault())
-            .Where(x => x != null)
-            .Where(x => (x!.Synonym.Length - 1) / x!.Synonym.Length <= x.Ratio)
+            .Where(x => x != null && x.Ratio >= threshold)
             .FirstOrDefault();
 
         if (match == null) return null;
